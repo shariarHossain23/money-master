8a75bd05aff3ee0d3127eb55cbc01d0a6dc1744b
shariarhossain23e <shariarhossain23@gamil.comm>
2022-02-16 23:23:29 +0600

    expense balance and total balance

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-document.getElementById("")
+document.getElementById("calculate-btn").addEventListener("click",function(){
+    // total money
+    const incomeMoney = inputField("income");
+
+    // expense Money
+    const foodMoney = inputField("food");
+    const rentMoney = inputField("rent");
+    const clothMoney = inputField("cloth");
+
+    // total expense Money
+    const totalExpenseMoney = parseFloat(foodMoney) + parseFloat(rentMoney) + parseFloat(clothMoney);
+
+    // totalBalance 
+    const totalBalance = parseFloat(incomeMoney) - totalExpenseMoney;
+    console.log(totalBalance)
+
+    // show user interface
+
+    // expense
+    document.getElementById("expense-money").innerText = totalExpenseMoney;
+    // balance
+    document.getElementById("total-balance").innerText = totalBalance;
+})
+
+function inputField(id){
+     if(isNaN(id)){
+         alert("number")
+     }
+     else{
+        return document.getElementById(id+"-money").value;
+     }
+}
